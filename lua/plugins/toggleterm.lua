aae153687298f167dba7d4c4c4516c365f5c03fe
peterfung <peterfung42@gmail.com>
2023-11-28 22:29:25 +0800

    add threme

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,16 @@
+return {
+    'akinsho/toggleterm.nvim', 
+    version = "*", 
+    config = function()
+    require('lualine').setup {
+      options = {
+        open_mapping = [[<c-\>]],
+        -- 打开新终端后自动进入插入模式
+        start_in_insert = true,
+        -- 在当前buffer的下方打开新终端
+        direction = 'float',
+      },
+    }
+  end
+
+}
